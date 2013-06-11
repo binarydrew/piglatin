793d3f295bb2d7de5e34c2139a188754eca1afa4
binarydrew <aftl84@gmail.com>
2013-06-11 01:56:21 +0000

    fixed wrong tictactoe folder and cleaned up piglatin of comments

--- piglatin.rb
+++ piglatin.rb
@@ -8,31 +8,22 @@ word.each_char do |x|
 
 
 	if x == "a" || x == "e" || x == "i" || x == "o" || x == "u"
-		break # after first vowel
-		# find a way to store unprocessed char into unpro
-		match << x # store matched vowel into match
+		break 
+		match << x 
 	else 
-		unmatch << x # store not-matched vowel into dmatch
-	#else puts "#{word}" + "ay" #for word start with a vowel
+		unmatch << x 
 	end
 end
 
-if word.empty?  # how else can i determine this? in a more ruby-like manner?
-	puts ""	
-elsif word == "nil"  # this does not  (why?)
-	puts "nil"
-else 
-	unpro = word[unmatch.length, (word.length - unmatch.length)]
-    puts "#{unpro}" + "#{unmatch.join}" + "ay"
-end
+	if word.empty? 
+		puts ""	
+	elsif word == "nil"  
+		puts "nil"
+	else 
+		unpro = word[unmatch.length, (word.length - unmatch.length)]
+	    puts "#{unpro}" + "#{unmatch.join}" + "ay"
+	end
 
-=begin
-some
-myArray = ['s', 'o', 'm', 'e']
 
-index = myArray[1]
-=end
-
-# how do i get the "thing" from position 1 to the end?
 
 
